f0b1521abe7731c0613b761251a1e6cce70d9d2c
Vipul Bhansali <vipulain81492@gmail.com>
2023-05-11 05:03:13 +0530

    Data Ingestion completed

--- sensor/logger.py
+++ sensor/logger.py
@@ -10,7 +10,7 @@ LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
 
 
 #Create folder if not available
-o.makedirs(LOG_FILE_DIR,exist_ok=True)
+os.makedirs(LOG_FILE_DIR,exist_ok=True)
 
 #log file path
 
